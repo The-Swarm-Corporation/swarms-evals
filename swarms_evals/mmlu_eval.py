fcae8a101b8db9e612126306de96c9d7463fb1d1
evelynmitchell <efmphone@gmail.com>
2024-06-16 07:29:28 -0600

    subject option for mmlu

--- swarms_evals/mmlu_eval.py
+++ swarms_evals/mmlu_eval.py
@@ -51,7 +51,7 @@ def normalize_answer(s):
 
 # Load MMLU dataset
 def load_mmlu_dataset():
-    dataset = load_dataset("cais/mmlu")
+    dataset = load_dataset("cais/mmlu", "all")
     test_data = dataset["test"]
     # Print the number of test examples
 
